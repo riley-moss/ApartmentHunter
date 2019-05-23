02458b04a54026fd1147a1e3a0c0edbc6e4a73d6
Riley-Pillar Mac <rmoss@pillartechnology.com>
2019-05-23 12:59:12 -0500

    Refactor test file name and tests

--- app/main/Scraper.py
+++ app/main/Scraper.py
@@ -1,4 +1,5 @@
 from bs4 import BeautifulSoup
 
+
 def get_html_text(url):
     return BeautifulSoup(open(url), 'html.parser')
---
+++ app/tests/ScraperUnitTest.py
@@ -0,0 +1,23 @@
+from unittest import TestCase, main as unittest_main
+from app.main import Scraper
+
+from bs4 import BeautifulSoup
+import os
+
+
+class TestScraper(TestCase):
+
+    def setUp(self):
+        self.fake_url = os.path.abspath('../tests/fake_placard.html')
+
+    def test_getHTMLTextReturnsAValue(self):
+        html_soup = ""
+        self.assertIsNotNone(html_soup)
+
+    def test_givenHTMLFileReturnsABeautifulSoupObject(self):
+        html_soup = Scraper.get_html_text(self.fake_url)
+        self.assertIs(type(html_soup), BeautifulSoup)
+
+
+if __name__ == '__main__':
+    unittest_main()
