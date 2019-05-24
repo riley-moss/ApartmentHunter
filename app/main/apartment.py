ad897ea59c7735d581b991a4fc8ce7b64a56b71f
Riley-Pillar Mac <rmoss@pillartechnology.com>
2019-05-24 09:55:42 -0500

    Refactored Apartment class to use @property to make variables private

--- app/main/apartment.py
+++ app/main/apartment.py
@@ -1,6 +1,18 @@
-class Apartment:
+class Apartment(object):
 
     def __init__(self, url, location, rent):
-        self.url = url
-        self.location = location
-        self.rent = rent
+        self._url = url
+        self._location = location
+        self._rent = rent
+
+    @property
+    def url(self):
+        return self._url
+
+    @property
+    def location(self):
+        return self._location
+
+    @property
+    def rent(self):
+        return self._rent
