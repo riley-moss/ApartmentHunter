be66f25c24bbd9f30fa01b49e73a69cb00bc1a50
Riley-Pillar Mac <rmoss@pillartechnology.com>
2019-05-24 09:01:46 -0500

    Refactored for opening test file in setup and closing in teardown

--- app/tests/ScraperUnitTest.py
+++ app/tests/ScraperUnitTest.py
@@ -12,27 +12,29 @@ class TestScraper(ut.TestCase):
         self.test_apartment_url = 'https://www.apartments.com/lakeview-3200-chicago-il/3qc6kde/'
         self.test_apartment_location = '3200 N Clark St, Chicago, IL 60657'
         self.test_apartment_rent = '$1,844 - 2,100'
+        self.test_response = open(self.test_placard_file)
 
     def test_givenBlankResponseReturnBlankString(self):
         html_soup = ''
         self.assertEqual('', Scraper.get_html_text(html_soup))
 
     def test_givenHTMLResponseReturnsABeautifulSoupObject(self):
-        response = open(self.test_placard_file)
-        html_soup = Scraper.get_html_text(response)
+        html_soup = Scraper.get_html_text(self.test_response)
         self.assertIs(type(html_soup), BeautifulSoup)
-        response.close()
 
     def test_givenBlankHTMLReturnEmptyList(self):
         self.assertEqual([], Scraper.parse_soup_for_placards(''))
 
     @ut.skip
     def test_givenHTMLWithOnePlacardReturnListWithOneApartment(self):
-        html_soup = Scraper.get_html_text(self.test_placard_file)
+        html_soup = Scraper.get_html_text(self.test_response)
         apartment = Scraper.parse_soup_for_placards(html_soup)[0]
         self.assertEqual(self.test_apartment_url, apartment.url)
         self.assertEqual(self.test_apartment_location, apartment.location)
         self.assertEqual(self.test_apartment_rent, apartment.rent)
+
+    def tearDown(self):
+        self.test_response.close()
 
 
 if __name__ == '__main__':
